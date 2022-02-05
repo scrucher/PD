1d257ed99c8e4eed24ad76f8aed30105f9cd0319
scrucher <jhonsteph666@gmail.com>
2022-02-05 12:11:00 +0000

    edit mngodb connection options

--- Config/config.ts
+++ Config/config.ts
@@ -1,8 +1,7 @@
-export const DbUrl = process.env.MONGODB_URI ||'mongodb+srv://scrucher:dark@life@2014@cluster0.vps4s.mongodb.net/food-delivery-db?retryWrites=true&w=majority&directConnection=true'
+export const DbUrl = process.env.MONGODB_URI ||'mongodb+srv://scrucher:dark@life@2014@cluster0.vps4s.mongodb.net/food-delivery-db?retryWrites=true&w=majority'
     console.log(process.env.MONGODB_URI)
 export const DbConnectionOptions = {
     autoIndex: true,
-    directConnection: true,
     autoCreate: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
